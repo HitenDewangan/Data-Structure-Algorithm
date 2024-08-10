90a9b9618cc93da0adb5bc88c9460791b838c27b
HIDED <148749063+HitenDewangan@users.noreply.github.com>
2024-08-10 17:12:43 +0530

    Delete .history/ClassWork/Priority Queue & Heap/Heap directory

--- .history/ClassWork/Priority Queue & Heap/Heap/Candies_20240808141828.py
+++
@@ -1,8 +0,0 @@
-'''
-T = 3
-A = [2, 4, 6, 8, 10]
-
-Every second you can pick any bucket i and eat all a[i] candies. After that bucket will be filled again with a[i]/2 (half of the original amount of candy).
-
-
-'''
