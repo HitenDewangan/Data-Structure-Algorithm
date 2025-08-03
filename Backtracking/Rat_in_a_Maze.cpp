6da3ac46f32389ce1a454fe7751d96a20f094aca
HIDED <hitendewangan100j@gmail.com>
2025-08-04 00:38:35 +0530

    Backtracking : Rat-in-a-maze

---
+++ Backtracking/Rat_in_a_Maze.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+void helper(vector<vector<int>>& maze, int r, int c, string path, vector<string>& ans, vector<vector<bool>>& vis) {
+    int n = maze.size();
+    // base case
+    if(r < 0 || c < 0 || r >= n || c >= n || maze[r][c] == 0 || vis[r][c] == true) {
+        return; 
+    }
+
+    if(r == n-1 && c == n-1) {
+        ans.push_back(path);
+        return;
+    }
+
+    vis[r][c] = true;
+    helper(maze, r+1, c, path+'D', ans, vis);
+    helper(maze, r-1, c, path+'U', ans, vis);
+    helper(maze, r, c-1, path+'L', ans, vis);
+    helper(maze, r, c+1, path+'R', ans, vis);
+    vis[r][c] = false;
+    
+}
+
+vector<string> findPaths(vector<vector<int>>& maze) {
+    int n = maze.size();
+    vector<string> ans;
+    string path = "";
+    vector<vector<bool>> vis(n, vector<bool>(n, false));
+
+    helper(maze, 0, 0, path, ans, vis);
+
+    return ans;
+
+}
+
+
+int main() {
+    // Example Rat in a Maze problem
+    vector<vector<int>> maze = {
+        {1, 0, 0, 0},
+        {1, 1, 0, 1},
+        {1, 1, 0, 0},
+        {0, 1, 1, 1}
+    };
+    
+    vector<string> paths = findPaths(maze);
+    cout << "Possible paths from start to end:" << endl;    
+    for (const string& path : paths) {
+        cout << path << endl;
+    }
+    
+    return 0;
+}
